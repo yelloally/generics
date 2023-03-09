26e890740ccb4853a0a6a0627c3ce6fe229834f1
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-09 20:08:00 +0100

    Add files via upload

---
+++ Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    //define a generic clas( type parameter 'T')
+    class Generics<T>
+    { 
+        private T myField;
+
+        public Generics(T value)
+        {
+            myField = value;
+        }
+
+        //return the value 
+        public T GetMyField()
+        {
+            return myField;
+        }
+    }
+
+    static void Main()
+    {
+        //create an instance with string 
+        Generics<string> myStringClass = new Generics<string>("Ka-pi-ba-ra! KAPIRABA KAPIRABA KAPARABA!!!");
+
+        //create an instance wiht int
+        Generics<int> myIntClass = new Generics<int>(11052005);
+
+        //call
+        Console.WriteLine(myStringClass.GetMyField());
+        Console.WriteLine(myIntClass.GetMyField());
+    }
+}
